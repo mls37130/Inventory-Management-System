e39f3e2698058cd440f7bf906c1ef2a10105e654
Megan Shepherd <33328112+mls37130@users.noreply.github.com>
2017-11-09 00:48:50 -0600

    Add files via upload

---
+++ Example4-Megan-s-Branch - Copy/Example4-Megan-s-Branch/InventoryManager/InventoryManager/Item.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager
+{
+    public class Item
+    {
+        //I feel like there's some datatype that allows for the creation of unique ID's....
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double WholesalePrice { get; set;}
+        public double RetailPrice { get; set; }
+        public int CurrentQuantity { get; set; }
+        public int MaxQuantity { get; set; }
+        public int MinQuatitiy { get; set; }
+        public DateTime AddedDate { get; set; }
+        public int MaxItemSold { get; set; }
+        public int MinItemSold { get; set; }
+        public int DaysThreshold { get; set; }
+        public double SaleDiscountAmt { get; set; }
+
+
+        public Item()
+        {         
+        }
+
+        public Item(string i, string n, string c)
+        {
+            Id = i;
+            Name = n;
+            Category = c;
+        }
+
+
+
+     public double CalculateROI()
+        {
+
+            //some logic used to calculate the ROI...could be based on sales or maybe profit margin??
+
+            return 0; 
+        }
+
+      public static void CalculateDiscount(DateTime dateApplied)
+        {
+            //iterates through all inventory items. 
+            // if date this method is run matches the date preset pricing solution is supposed to be applied for an item (if statement), 
+            // it calculates the discounted price using discount rate and sets new price to discount price.
+
+            
+        }
+
+        public override string ToString()
+        {
+
+
+            //probably better to have an option on the inventory page for showing "item detail" instead of showing all info
+            //return $"{Id}, {Name}, {Category}, WP: {WholesalePrice}, RP: {RetailPrice}, CurQ: {CurrentQuantity}, MaxQ: {MaxQuantity}, MinQ: {MinQuatitiy}, Added: {AddedDate}, MaxQSold: {MaxItemSold}, MinQSold: {MinItemSold}, DaysThres: {DaysThreshold}, Dis: {SaleDiscountAmt}";
+            return $"{Id}, {Name}, {Category}, WP: {WholesalePrice}, RP: {RetailPrice}"; 
+        }
+
+
+    
+
+    }
+}
---
+++ Example4-Megan-s-Branch - Copy/Example4-Megan-s-Branch/InventoryManager/InventoryManager/LoginForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace InventoryManager
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.managerLoginButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.createUserLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(136, 66);
+            this.userTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(89, 22);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(136, 109);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(89, 22);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User Login:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(50, 142);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(145, 26);
+            this.loginButton.TabIndex = 8;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // managerLoginButton
+            // 
+            this.managerLoginButton.Location = new System.Drawing.Point(40, 221);
+            this.managerLoginButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.managerLoginButton.Name = "managerLoginButton";
+            this.managerLoginButton.Size = new System.Drawing.Size(164, 23);
+            this.managerLoginButton.TabIndex = 5;
+            this.managerLoginButton.Text = "I\'m a Manager";
+            this.managerLoginButton.UseVisualStyleBackColor = true;
+            this.managerLoginButton.Click += new System.EventHandler(this.managerLoginButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.createUserLinkLabel);
+            this.groupBox1.Location = new System.Drawing.Point(11, 6);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(228, 200);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Login";
+            // 
+            // createUserLinkLabel
+            // 
+            this.createUserLinkLabel.AutoSize = true;
+            this.createUserLinkLabel.Location = new System.Drawing.Point(63, 173);
+            this.createUserLinkLabel.Name = "createUserLinkLabel";
+            this.createUserLinkLabel.Size = new System.Drawing.Size(105, 17);
+            this.createUserLinkLabel.TabIndex = 0;
+            this.createUserLinkLabel.TabStop = true;
+            this.createUserLinkLabel.Text = "Create Account";
+            this.createUserLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.createUserLinkLabel_LinkClicked);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 338);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.managerLoginButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button managerLoginButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel createUserLinkLabel;
+    }
+}
